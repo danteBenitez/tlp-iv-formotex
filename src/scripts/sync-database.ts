498d83f944b8184735174a487c9956ccaffd1d8f
danteBenitez <benitezdante123@gmail.com>
2024-09-09 14:41:12 -0300

    chore: Agregado script para sincronizar base de datos

---
+++ src/scripts/sync-database.ts
@@ -0,0 +1,31 @@
+import { createInterface } from "readline/promises";
+import { sequelize } from "../database/connection.js";
+import { setupDatabase } from "../database/setup.js";
+
+export async function syncDatabase() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const answer = await rl.question(`¿Está seguro que desea forzar la sincronización de base de datos?\nEsta acción podría eliminar datos ya guardados. (Y/n)`);
+
+    try {
+        if (answer == "Y") {
+            console.log("Sincronizando base de datos...");
+            await setupDatabase();
+            await sequelize.sync({
+                force: true
+            })
+            console.log("Sincronización exitosa");
+        }
+
+    } catch (err) {
+        console.error("Hubo un error en la sincronización", err);
+    } finally {
+        rl.close();
+        sequelize.close();
+    }
+}
+
+await syncDatabase();
