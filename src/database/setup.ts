6c0aa6543691f2878531ef769838ac4abe76a7fe
danteBenitez <benitezdante123@gmail.com>
2024-09-09 14:41:43 -0300

    fix: Eliminada sincronización de DB de la configuración

--- src/database/setup.ts
+++ src/database/setup.ts
@@ -1,4 +1,3 @@
-import { config } from "../config/config.service.js";
 import { defineRelations } from "../models/relations.js";
 import { sequelize } from "./connection.js";
 import { seedDatabase } from "./seed/index.js";
@@ -6,9 +5,6 @@ import { seedDatabase } from "./seed/index.js";
 export async function setupDatabase() {
     defineRelations();
 
-    await sequelize.sync({
-        force: config.getDatabaseOptions().SHOULD_FORCE
-    })
     await seedDatabase()
         .then(() => console.log("Insertados roles correctamente"));
 
