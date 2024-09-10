d4345f1b37d7932f9242cf63368b8b21e371224f
danteBenitez <benitezdante123@gmail.com>
2024-09-09 23:17:12 -0300

    chores: Usar funcionalidad `as const` de TypeScript

--- src/consts/roles.ts
+++ src/consts/roles.ts
@@ -1,10 +1,16 @@
 export const ROLES = {
     ADMIN: "admin",
     USER: "user"
-}
+} as const;
+
+export type RoleName = typeof ROLES[keyof typeof ROLES];
 
 /** Roles permitidos a ser asignados a usuarios no administradores */
-export const ALLOWED_ROLES = [ROLES.USER];
+export const ALLOWED_ROLES = [ROLES.USER] as const;
+
+export type AssignableRoles = typeof ALLOWED_ROLES[keyof typeof ALLOWED_ROLES];
 
 /** Roles permitidos a ser asignados por administradores */
 export const ADMIN_ALLOWED_ROLES = [ROLES.USER, ROLES.ADMIN];
+
+export type AdminAssignableRoles = typeof ADMIN_ALLOWED_ROLES[keyof typeof ADMIN_ALLOWED_ROLES];
